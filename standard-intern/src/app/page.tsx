f7201d8cdbe871b175d23309906eb6e174222655
timons-AI <123802219+timons-AI@users.noreply.github.com>
2023-06-22 01:25:39 +0300

    Update page.tsx
    ff

--- standard-intern/src/app/page.tsx
+++ standard-intern/src/app/page.tsx
@@ -1,12 +1,10 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p className=' text-4xl font-extrabold bg-clip-text bg-gradient-to-r from-blue-400 to-emerald-500 text-transparent capitalize'>
+      <p className=' text-4xl font-extrabold bg-clip-text bg-gradient-to-r from-blue-400 to-emerald-400 text-transparent capitalize'>
         This place smells like butter bacon eggs and sausage and cheese.
       </p>
-      <p className=' text-4xl font-extrabold bg-clip-text bg-gradient-to-r from-blue-400 to-emerald-500 text-transparent capitalize'>
+      <p className=' text-4xl font-extrabold bg-clip-text bg-gradient-to-r from-orange-400 to-rose-500 text-transparent capitalize'>
         I'm gonna die here.
       </p>
 
