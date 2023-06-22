3a143d8d98c3b70522f436929a28bb75e7a7cef6
mrbillionn <kyaligonzawarren44@gmail.con>
2023-06-22 20:33:56 +0300

    Update page.tsx

--- standard-intern/src/app/page.tsx
+++ standard-intern/src/app/page.tsx
@@ -2,10 +2,10 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <p className=' text-4xl font-extrabold bg-clip-text bg-gradient-to-r from-blue-400 to-emerald-400 text-transparent capitalize'>
-        This place is about to get funcky.
+        Standard Intern Pro
       </p>
       <p className=' text-4xl font-extrabold bg-clip-text bg-gradient-to-r from-orange-400 to-rose-500 text-transparent capitalize'>
-        I'm gonna die here.
+        This place is about to get funcky.
       </p>
 
     </main>
