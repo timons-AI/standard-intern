bf7dc19fe1d410cceee858de2483402a648722e2
mrbillionn <kyaligonzawarren44@gmail.con>
2023-06-22 20:29:56 +0300

    Update page.tsx

--- standard-intern/src/app/page.tsx
+++ standard-intern/src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <p className=' text-4xl font-extrabold bg-clip-text bg-gradient-to-r from-blue-400 to-emerald-400 text-transparent capitalize'>
-        This place smells like butter bacon eggs and sausage and cheese.
+        This place is about to get funcky.
       </p>
       <p className=' text-4xl font-extrabold bg-clip-text bg-gradient-to-r from-orange-400 to-rose-500 text-transparent capitalize'>
         I'm gonna die here.
