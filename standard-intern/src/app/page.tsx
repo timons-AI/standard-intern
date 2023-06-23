9a27686b86546d32e954f68124ce532295adebdc
mrbillionn <kyaligonzawarren44@gmail.con>
2023-06-23 22:47:01 +0300

    Update page.tsx

--- standard-intern/src/app/page.tsx
+++ standard-intern/src/app/page.tsx
@@ -2,25 +2,25 @@ import Head from 'next/head';
 
 const Home = () => {
   return (
-    <div className="bg-gray-900 text-white p-5">
+    <div className="bg-gray-100 text-black p-5">
       <Head>
         <title>Home</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1 className="text-blue-500 font-bold text-4xl mt-10 bg-gradient-to-r from-blue-500 to-purple-500 text-white py-2 px-4 rounded-lg">
+      <h1 className="text-red-400 font-bold text-4xl mt-10 bg-gradient-to-r from-red-400 to-pink-400 text-white py-2 px-4 rounded-lg">
         Standard Intern
       </h1>
 
-      <h2 className="text-gray-700 font-bold text-2xl mt-5">Find your best places</h2>
+      <h2 className="text-gray-800 font-bold text-2xl mt-5">Find your best places</h2>
 
-      <div className="bg-gray-800 rounded-md p-2 flex items-center mt-5">
+      <div className="bg-gray-200 rounded-md p-2 flex items-center mt-5">
         <input
-          className="font-bold text-xl flex-1 py-1 px-2 border-none outline-none bg-gray-800 text-white"
+          className="font-bold text-xl flex-1 py-1 px-2 border-none outline-none bg-gray-200 text-black"
           type="text"
           placeholder="What are you looking for?"
         />
-        <button className="bg-blue-500 w-8 h-8 rounded-md flex items-center justify-center">
+        <button className="bg-blue-400 w-8 h-8 rounded-md flex items-center justify-center">
           <img
             src="/images/search.png"
             alt="Search"
@@ -29,68 +29,68 @@ const Home = () => {
         </button>
       </div>
 
-      <h2 className="text-gray-700 font-bold text-xl mt-8">Most Popular</h2>
+      <h2 className="text-gray-800 font-bold text-xl mt-8">Most Popular</h2>
 
       <div className="flex overflow-x-auto mt-5">
-        <div className="flex-shrink-0 bg-gradient-to-r from-red-500 to-yellow-500 h-96 w-72 rounded-lg p-3 mx-2">
+        <div className="flex-shrink-0 bg-gradient-to-r from-yellow-400 to-green-400 h-96 w-72 rounded-lg p-3 mx-2">
           <img
             src="/images/dev.png"
             alt="Software Developer"
             className="w-16 h-16 rounded-full"
           />
           <div className="flex flex-col mt-4">
-            <div className="text-gray-700 font-bold text-lg">Software Developer</div>
-            <div className="text-blue-700 font-bold mt-1">Full time</div>
-            <div className="text-gray-700 mt-1">Location: San Francisco</div>
-            <div className="text-gray-700 mt-1">People Needed: 5</div>
-            <div className="text-gray-700 font-bold mt-auto">$50/h</div>
-            <button className="bg-blue-500 text-white mt-4 py-2 px-4 rounded-lg">
+            <div className="text-gray-800 font-bold text-lg">Software Developer</div>
+            <div className="text-blue-600 font-bold mt-1">Full time</div>
+            <div className="text-gray-800 mt-1">Location: San Francisco</div>
+            <div className="text-gray-800 mt-1">People Needed: 5</div>
+            <div className="text-gray-800 font-bold mt-auto">$50/h</div>
+            <button className="bg-blue-400 text-white mt-4 py-2 px-4 rounded-lg">
               Apply
             </button>
           </div>
         </div>
 
-        <div className="flex-shrink-0 bg-gradient-to-r from-green-500 to-blue-500 h-96 w-72 rounded-lg p-3 mx-2">
+        <div className="flex-shrink-0 bg-gradient-to-r from-blue-400 to-indigo-400 h-96 w-72 rounded-lg p-3 mx-2">
           <img
             src="/images/accountant.png"
             alt="Accountant"
             className="w-16 h-16 rounded-full"
           />
           <div className="flex flex-col mt-4">
-            <div className="text-gray-700 font-bold text-lg">Accountant</div>
-            <div className="text-gray-700 font-bold mt-1">Full time</div>
-            <div className="text-gray-700 mt-1">Location: New York</div>
-            <div className="text-gray-700 mt-1">People Needed: 3</div>
-            <div className="text-gray-700 font-bold mt-auto">$50/h</div>
-            <button className="bg-blue-500 text-white mt-4 py-2 px-4 rounded-lg">
+            <div className="text-gray-800 font-bold text-lg">Accountant</div>
+            <div className="text-gray-800 font-bold mt-1">Full time</div>
+            <div className="text-gray-800 mt-1">Location: New York</div>
+            <div className="text-gray-800 mt-1">People Needed: 3</div>
+            <div className="text-gray-800 font-bold mt-auto">$50/h</div>
+            <button className="bg-blue-400 text-white mt-4 py-2 px-4 rounded-lg">
               Apply
             </button>
           </div>
         </div>
 
-        <div className="flex-shrink-0 bg-gradient-to-r from-purple-500 to-indigo-500 h-96 w-72 rounded-lg p-3 mx-2">
+        <div className="flex-shrink-0 bg-gradient-to-r from-purple-400 to-indigo-400 h-96 w-72 rounded-lg p-3 mx-2">
           <img
             src="/images/driver.png"
             alt="Car Driver"
             className="w-16 h-16 rounded-full"
           />
           <div className="flex flex-col mt-4">
-            <div className="text-gray-700 font-bold text-lg">Car driver</div>
-            <div className="text-blue-700 font-bold mt-1">Full time</div>
-            <div className="text-gray-700 mt-1">Location: Los Angeles</div>
-            <div className="text-gray-700 mt-1">People Needed: 2</div>
-            <div className="text-gray-700 font-bold mt-auto">$50/h</div>
-            <button className="bg-blue-500 text-white mt-4 py-2 px-4 rounded-lg">
+            <div className="text-gray-800 font-bold text-lg">Car driver</div>
+            <div className="text-blue-600 font-bold mt-1">Full time</div>
+            <div className="text-gray-800 mt-1">Location: Los Angeles</div>
+            <div className="text-gray-800 mt-1">People Needed: 2</div>
+            <div className="text-gray-800 font-bold mt-auto">$50/h</div>
+            <button className="bg-blue-400 text-white mt-4 py-2 px-4 rounded-lg">
               Apply
             </button>
           </div>
         </div>
       </div>
 
-      <h2 className="text-gray-700 font-bold text-xl mt-8">Nearby jobs</h2>
+      <h2 className="text-gray-800 font-bold text-xl mt-8">Nearby jobs</h2>
 
       <div className="mt-5">
-        <div className="bg-gradient-to-r from-pink-500 to-red-500 rounded-lg flex items-center p-5 mt-3">
+        <div className="bg-gradient-to-r from-red-400 to-pink-400 rounded-lg flex items-center p-5 mt-3">
           <img
             src="/images/company1_logo.png"
             alt="Company Logo"
@@ -99,18 +99,18 @@ const Home = () => {
           <div className="flex-1 ml-5">
             <div className="font-bold text-xl">Graphic Designer</div>
             <div className="flex items-center mt-1">
-              <div className="text-gray-700 font-bold mr-2">Company: ABC Designs</div>
-              <div className="bg-gray-300 rounded-md flex items-center px-2 py-1">
+              <div className="text-gray-800 font-bold mr-2">Company: ABC Designs</div>
+              <div className="bg-gray-200 rounded-md flex items-center px-2 py-1">
               </div>
-              <div className="text-gray-700 font-bold mt-1">$5/h</div>
+              <div className="text-gray-800 font-bold mt-1">$5/h</div>
             </div>
           </div>
-          <button className="bg-blue-500 text-white mt-4 py-2 px-4 rounded-lg ml-auto">
+          <button className="bg-blue-400 text-white mt-4 py-2 px-4 rounded-lg ml-auto">
             Apply
           </button>
         </div>
 
-        <div className="bg-gradient-to-r from-yellow-500 to-green-500 rounded-lg flex items-center p-5 mt-3">
+        <div className="bg-gradient-to-r from-yellow-400 to-green-400 rounded-lg flex items-center p-5 mt-3">
           <img
             src="/images/company2_logo.png"
             alt="Company Logo"
@@ -119,18 +119,18 @@ const Home = () => {
           <div className="flex-1 ml-5">
             <div className="font-bold text-xl">Nurse</div>
             <div className="flex items-center mt-1">
-              <div className="text-gray-700 font-bold mr-2">Company: XYZ Healthcare</div>
-              <div className="bg-gray-300 rounded-md flex items-center px-2 py-1">
+              <div className="text-gray-800 font-bold mr-2">Company: XYZ Healthcare</div>
+              <div className="bg-gray-200 rounded-md flex items-center px-2 py-1">
               </div>
-              <div className="text-gray-700 font-bold mt-1">$10/h</div>
+              <div className="text-gray-800 font-bold mt-1">$10/h</div>
             </div>
           </div>
-          <button className="bg-blue-500 text-white mt-4 py-2 px-4 rounded-lg ml-auto">
+          <button className="bg-blue-400 text-white mt-4 py-2 px-4 rounded-lg ml-auto">
             Apply
           </button>
         </div>
 
-        <div className="bg-gradient-to-r from-blue-500 to-indigo-500 rounded-lg flex items-center p-5 mt-3">
+        <div className="bg-gradient-to-r from-blue-400 to-indigo-400 rounded-lg flex items-center p-5 mt-3">
           <img
             src="/images/company3_logo.png"
             alt="Company Logo"
@@ -139,18 +139,18 @@ const Home = () => {
           <div className="flex-1 ml-5">
             <div className="font-bold text-xl">Chef</div>
             <div className="flex items-center mt-1">
-              <div className="text-gray-700 font-bold mr-2">Company: Foodie Delights</div>
-              <div className="bg-gray-300 rounded-md flex items-center px-2 py-1">
+              <div className="text-gray-800 font-bold mr-2">Company: Foodie Delights</div>
+              <div className="bg-gray-200 rounded-md flex items-center px-2 py-1">
               </div>
-              <div className="text-gray-700 font-bold mt-1">$8/h</div>
+              <div className="text-gray-800 font-bold mt-1">$8/h</div>
             </div>
           </div>
-          <button className="bg-blue-500 text-white mt-4 py-2 px-4 rounded-lg ml-auto">
+          <button className="bg-blue-400 text-white mt-4 py-2 px-4 rounded-lg ml-auto">
             Apply
           </button>
         </div>
 
-        <div className="bg-gradient-to-r from-yellow-500 to-green-500 rounded-lg flex items-center p-5 mt-3">
+        <div className="bg-gradient-to-r from-yellow-400 to-green-400 rounded-lg flex items-center p-5 mt-3">
           <img
             src="/images/company4_logo.png"
             alt="Company Logo"
@@ -159,13 +159,13 @@ const Home = () => {
           <div className="flex-1 ml-5">
             <div className="font-bold text-xl">Baby Sitter</div>
             <div className="flex items-center mt-1">
-              <div className="text-gray-700 font-bold mr-2">Company: Tiny Tots Care</div>
-              <div className="bg-gray-300 rounded-md flex items-center px-2 py-1">
+              <div className="text-gray-800 font-bold mr-2">Company: Tiny Tots Care</div>
+              <div className="bg-gray-200 rounded-md flex items-center px-2 py-1">
               </div>
-              <div className="text-gray-700 font-bold mt-1">$6/h</div>
+              <div className="text-gray-800 font-bold mt-1">$6/h</div>
             </div>
           </div>
-          <button className="bg-blue-500 text-white mt-4 py-2 px-4 rounded-lg ml-auto">
+          <button className="bg-blue-400 text-white mt-4 py-2 px-4 rounded-lg ml-auto">
             Apply
           </button>
         </div>
