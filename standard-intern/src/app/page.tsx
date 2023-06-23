fa1525c50cb0e1c0ace7d38998ac80f3b0108ee9
mrbillionn <kyaligonzawarren44@gmail.con>
2023-06-23 19:45:32 +0300

    Update page.tsx

--- standard-intern/src/app/page.tsx
+++ standard-intern/src/app/page.tsx
@@ -1,38 +1,43 @@
-import React from 'react';
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <div className="text-center mb-12">
-        <h1 className="text-5xl font-extrabold text-gray-800">Welcome to Standard Intern Pro</h1>
-        <p className="text-2xl text-gray-600 mt-4">Transform your career with valuable internships</p>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="flex items-center justify-between mb-8">
+        <p className="text-4xl font-extrabold bg-clip-text bg-gradient-to-r from-blue-400 to-emerald-400 text-transparent capitalize">
+          Standard Intern Pro
+        </p>
+        <p className="text-4xl font-extrabold bg-clip-text bg-gradient-to-r from-orange-400 to-rose-500 text-transparent capitalize">
+          The Standard Corporation.
+        </p>
       </div>
       <div className="flex flex-wrap justify-center">
-        {/* Animated Feature Section */}
-        <div className="flex items-center justify-center bg-white rounded-lg shadow-lg p-6 mb-4 mx-2 w-80">
-          <div className="w-16 h-16 bg-gray-200 rounded-full mb-4 animate-pulse" />
-          <div className="flex flex-col ml-4">
-            <h3 className="text-lg font-semibold">Gain Real-World Experience</h3>
-            <p className="text-gray-500 mt-2">Work on impactful projects with top companies in your field.</p>
-          </div>
-        </div>
-        {/* Add more animated feature sections here */}
-      </div>
-      <div className="flex flex-wrap justify-center mt-8">
-        {/* Testimonials */}
-        <div className="flex items-center justify-center bg-white rounded-lg shadow-lg p-6 mb-4 mx-2 w-80">
+        {/* Top Companies */}
+        <div className="flex items-center justify-center bg-white rounded-lg shadow-lg p-6 mb-4 mx-2 w-64">
           <div className="w-16 h-16 bg-gray-200 rounded-full mb-4" />
           <div className="flex flex-col ml-4">
-            <h3 className="text-lg font-semibold">John Doe</h3>
-            <p className="text-gray-500 mt-2">"Standard Intern Pro helped me land my dream internship and kickstart my career."</p>
+            <h3 className="text-lg font-semibold">Company Name</h3>
+            <p className="text-gray-500">Salary: $5000</p>
+            <p className="text-gray-500">Duration: 3 months</p>
+            <button className="bg-blue-500 text-white px-4 py-2 mt-4 rounded-md">
+              Apply
+            </button>
           </div>
         </div>
-        {/* Add more testimonial sections here */}
+        {/* Add more company cards here */}
       </div>
-      <div className="flex justify-center mt-12">
-        <button className="bg-blue-500 text-white px-6 py-3 rounded-md text-lg font-semibold">
-          Apply Now
-        </button>
+      <div className="flex flex-wrap justify-center mt-8">
+        {/* Suggested Companies */}
+        <div className="flex items-center justify-center bg-white rounded-lg shadow-lg p-6 mb-4 mx-2 w-64">
+          <div className="w-16 h-16 bg-gray-200 rounded-full mb-4" />
+          <div className="flex flex-col ml-4">
+            <h3 className="text-lg font-semibold">Company Name</h3>
+            <p className="text-gray-500">Salary: $5000</p>
+            <p className="text-gray-500">Duration: 3 months</p>
+            <button className="bg-blue-500 text-white px-4 py-2 mt-4 rounded-md">
+              Apply
+            </button>
+          </div>
+        </div>
+        {/* Add more suggested company cards here */}
       </div>
     </main>
   );
