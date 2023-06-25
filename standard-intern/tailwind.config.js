42480953e44be8044352689b274b9959dd2a50d3
mrbillionn <kyaligonzawarren44@gmail.con>
2023-06-25 12:54:39 +0300

    tailwind effects  tailwind.config

--- standard-intern/tailwind.config.js
+++ standard-intern/tailwind.config.js
@@ -11,8 +11,12 @@ module.exports = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-diagonal-orange': 'linear-gradient(45deg, #FFA500, #FF4500)',
+      },
+      boxShadow: {
+        'glass': '0 2px 6px rgba(0, 0, 0, 0.2)',
       },
     },
   },
   plugins: [],
-}
+};
