7c385702039a1e71804d128f865919240b23ae9a
mrbillionn <kyaligonzawarren44@gmail.con>
2023-06-25 12:41:09 +0300

    Update page.tsx

--- standard-intern/src/app/page.tsx
+++ standard-intern/src/app/page.tsx
@@ -84,7 +84,7 @@ const Home = () => {
               Apply
             </button>
           </div>
-          
+
 <div className="flex-shrink-0 bg-white h-96 w-72 rounded-lg p-3 mx-2">
   <img
     src="/images/marketing.png"
@@ -175,7 +175,7 @@ const Home = () => {
   </div> 
     </div>
       </div>
-
+      </div>
       <h2 className="text-gray-800 font-bold text-xl mt-8">Nearby jobs</h2>
 
       <div className="mt-5">
@@ -260,7 +260,7 @@ const Home = () => {
         </div>
       </div>
     </div>
-    </div>
+
   );
 };
 
