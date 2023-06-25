f851b96c75a799894332bafa737eda0c215aec07
mrbillionn <kyaligonzawarren44@gmail.con>
2023-06-25 12:18:35 +0300

    Update page.tsx

--- standard-intern/src/app/page.tsx
+++ standard-intern/src/app/page.tsx
@@ -84,93 +84,12 @@ const Home = () => {
               Apply
             </button>
           </div>
-        </div>
-      </div>
-
-      <h2 className="text-gray-800 font-bold text-xl mt-8">Nearby jobs</h2>
-
-      <div className="mt-5">
-        <div className="bg-white rounded-lg flex items-center p-5 mt-3">
           <img
-            src="/images/company1_logo.png"
-            alt="Company Logo"
-            className="w-16 h-16 rounded-full"
-          />
-          <div className="flex-1 ml-5">
-            <div className="font-bold text-xl">Graphic Designer</div>
-            <div className="flex items-center mt-1">
-              <div className="text-gray-800 font-bold mr-2">Company: ABC Designs</div>
-              <div className="bg-gray-200 rounded-md flex items-center px-2 py-1">
-              </div>
-              <div className="text-gray-800 font-bold mt-1">$5/h</div>
-            </div>
-          </div>
-          <button className="bg-black text-white mt-4 py-2 px-4 rounded-lg ml-auto">
-            Apply
-          </button>
-        </div>
-
-        <div className="bg-white rounded-lg flex items-center p-5 mt-3">
-          <img
-            src="/images/company2_logo.png"
-            alt="Company Logo"
-            className="w-16 h-16 rounded-full"
-          />
-          <div className="flex-1 ml-5">
-            <div className="font-bold text-xl">Nurse</div>
-            <div className="flex items-center mt-1">
-              <div className="text-gray-800 font-bold mr-2">Company: XYZ Healthcare</div>
-              <div className="bg-gray-200 rounded-md flex items-center px-2 py-1">
-              </div>
-              <div className="text-gray-800 font-bold mt-1">$10/h</div>
-            </div>
-          </div>
-          <button className="bg-black text-white mt-4 py-2 px-4 rounded-lg ml-auto">
-            Apply
-          </button>
-        </div>
-
-        <div className="bg-white rounded-lg flex items-center p-5 mt-3">
-          <img
-            src="/images/company3_logo.png"
-            alt="Company Logo"
-            className="w-16 h-16 rounded-full"
-          />
-          <div className="flex-1 ml-5">
-            <div className="font-bold text-xl">Chef</div>
-            <div className="flex items-center mt-1">
-              <div className="text-gray-800 font-bold mr-2">Company: Foodie Delights</div>
-              <div className="bg-gray-200 rounded-md flex items-center px-2 py-1">
-              </div>
-              <div className="text-gray-800 font-bold mt-1">$8/h</div>
-            </div>
-          </div>
-          <button className="bg-black text-white mt-4 py-2 px-4 rounded-lg ml-auto">
-            Apply
-          </button>
-        </div>
-
-        <div className="bg-white rounded-lg flex items-center p-5 mt-3">
-          <img
-            src="/images/company4_logo.png"
-            alt="Company Logo"
-            className="w-16 h-16 rounded-full"
-          />
-          <div className="flex-1 ml-5">
-            <div className="font-bold text-xl">Baby Sitter</div>
-            <div className="flex items-center mt-1">
-              <div className="text-gray-800 font-bold mr-2">Company: Tiny Tots Care</div>
-              <div className="bg-gray-200 rounded-md flex items-center px-2 py-1">
-              </div>
-              <div className="text-gray-800 font-bold mt-1">$6/h</div>
-            </div>
-          </div>
-          <button className="bg-black text-white mt-4 py-2 px-4 rounded-lg ml-auto">
-            Apply
-          </button>
-        </div>
-        
-        <div className="flex flex-col mt-4">
+    src="/images/designer.png"
+    alt="Graphic Designer"
+    className="w-16 h-16 rounded-full"
+  />
+  <div className="flex flex-col mt-4">
     <div className="text-gray-800 font-bold text-lg">Graphic Designer</div>
     <div className="text-blue-600 font-bold mt-1">Part time</div>
     <div className="text-gray-800 mt-1">Location: Chicago</div>
@@ -269,7 +188,92 @@ const Home = () => {
     <button className="bg-black text-white mt-4 py-2 px-4 rounded-lg">
       Apply
     </button>
-  </div>
+  </div> 
+        </div>
+      </div>
+
+      <h2 className="text-gray-800 font-bold text-xl mt-8">Nearby jobs</h2>
+
+      <div className="mt-5">
+        <div className="bg-white rounded-lg flex items-center p-5 mt-3">
+          <img
+            src="/images/company1_logo.png"
+            alt="Company Logo"
+            className="w-16 h-16 rounded-full"
+          />
+          <div className="flex-1 ml-5">
+            <div className="font-bold text-xl">Graphic Designer</div>
+            <div className="flex items-center mt-1">
+              <div className="text-gray-800 font-bold mr-2">Company: ABC Designs</div>
+              <div className="bg-gray-200 rounded-md flex items-center px-2 py-1">
+              </div>
+              <div className="text-gray-800 font-bold mt-1">$5/h</div>
+            </div>
+          </div>
+          <button className="bg-black text-white mt-4 py-2 px-4 rounded-lg ml-auto">
+            Apply
+          </button>
+        </div>
+
+        <div className="bg-white rounded-lg flex items-center p-5 mt-3">
+          <img
+            src="/images/company2_logo.png"
+            alt="Company Logo"
+            className="w-16 h-16 rounded-full"
+          />
+          <div className="flex-1 ml-5">
+            <div className="font-bold text-xl">Nurse</div>
+            <div className="flex items-center mt-1">
+              <div className="text-gray-800 font-bold mr-2">Company: XYZ Healthcare</div>
+              <div className="bg-gray-200 rounded-md flex items-center px-2 py-1">
+              </div>
+              <div className="text-gray-800 font-bold mt-1">$10/h</div>
+            </div>
+          </div>
+          <button className="bg-black text-white mt-4 py-2 px-4 rounded-lg ml-auto">
+            Apply
+          </button>
+        </div>
+
+        <div className="bg-white rounded-lg flex items-center p-5 mt-3">
+          <img
+            src="/images/company3_logo.png"
+            alt="Company Logo"
+            className="w-16 h-16 rounded-full"
+          />
+          <div className="flex-1 ml-5">
+            <div className="font-bold text-xl">Chef</div>
+            <div className="flex items-center mt-1">
+              <div className="text-gray-800 font-bold mr-2">Company: Foodie Delights</div>
+              <div className="bg-gray-200 rounded-md flex items-center px-2 py-1">
+              </div>
+              <div className="text-gray-800 font-bold mt-1">$8/h</div>
+            </div>
+          </div>
+          <button className="bg-black text-white mt-4 py-2 px-4 rounded-lg ml-auto">
+            Apply
+          </button>
+        </div>
+
+        <div className="bg-white rounded-lg flex items-center p-5 mt-3">
+          <img
+            src="/images/company4_logo.png"
+            alt="Company Logo"
+            className="w-16 h-16 rounded-full"
+          />
+          <div className="flex-1 ml-5">
+            <div className="font-bold text-xl">Baby Sitter</div>
+            <div className="flex items-center mt-1">
+              <div className="text-gray-800 font-bold mr-2">Company: Tiny Tots Care</div>
+              <div className="bg-gray-200 rounded-md flex items-center px-2 py-1">
+              </div>
+              <div className="text-gray-800 font-bold mt-1">$6/h</div>
+            </div>
+          </div>
+          <button className="bg-black text-white mt-4 py-2 px-4 rounded-lg ml-auto">
+            Apply
+          </button>
+        </div>
       </div>
     </div>
   );
