c9dade00315bcd4bbb9cc7db65abc373aa76b5a9
mrbillionn <kyaligonzawarren44@gmail.con>
2023-06-28 13:11:40 +0300

    Update footer.tsx

--- standard-intern/src/app/Components/footer.tsx
+++ standard-intern/src/app/Components/footer.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
 
-const footer = () => {
+const Footer = () => {
   return (
-    <div>footer</div>
-  )
-}
+    <footer className="bg-gray-800">
+      <div className="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-center items-center">
+          <p className="text-gray-300">Standard Corporation &copy; {new Date().getFullYear()}</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
-export default footer
+export default Footer;
