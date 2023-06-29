aa59711f94693399b06841b413e543739eea2858
mrbillionn <kyaligonzawarren44@gmail.con>
2023-06-29 11:49:24 +0300

    Update Navbar.tsx

--- standard-intern/src/app/Navbar/Navbar.tsx
+++ standard-intern/src/app/Navbar/Navbar.tsx
@@ -14,13 +14,16 @@ const Navbar = () => {
       <div className="px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
-{/* include logo code */}
+            {/* Logo */}
+            <div className="flex-shrink-0">
+              <span className="text-white font-bold">Standard</span>
+            </div>
           </div>
           <div className="md:hidden">
             <button
               className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
               onClick={toggleMenu}
-            >Search
+            >Button
               <svg
                 className="h-6 w-6"
                 xmlns="http://www.w3.org/2000/svg"
@@ -38,13 +41,39 @@ const Navbar = () => {
             </button>
           </div>
           <div className={`hidden md:flex ${isMenuOpen ? 'flex' : 'hidden'}`}>
-            {/* include menu items code */}
+            {/* Menu items */}
+            <ul className="md:flex items-center space-x-4">
+              <li>
+                <a className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium" href="#">
+                  Menu Item 1
+                </a>
+              </li>
+              <li>
+                <a className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium" href="#">
+                  Menu Item 2
+                </a>
+              </li>
+              <li>
+                <a className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium" href="#">
+                  Menu Item 3
+                </a>
+              </li>
+            </ul>
           </div>
         </div>
         {isMenuOpen && (
           <div className={`md:hidden ${isMenuOpen ? 'block' : 'hidden'}`}>
+            {/* Side menu items */}
             <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-           {/* include menu code */}
+              <a className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium" href="#">
+                Menu Item 1
+              </a>
+              <a className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium" href="#">
+                Menu Item 2
+              </a>
+              <a className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium" href="#">
+                Menu Item 3
+              </a>
             </div>
           </div>
         )}
