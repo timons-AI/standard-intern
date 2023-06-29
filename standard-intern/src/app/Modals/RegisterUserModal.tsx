4240c0c4fd75dad2983d442120e3f65f4f25f835
mrbillionn <kyaligonzawarren44@gmail.con>
2023-06-29 23:04:56 +0300

    Update RegisterUserModal.tsx

--- standard-intern/src/app/Modals/RegisterUserModal.tsx
+++ standard-intern/src/app/Modals/RegisterUserModal.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import React from 'react';
 
 type RegisterUserModalProps = {
@@ -9,11 +7,21 @@ type RegisterUserModalProps = {
 
 const RegisterUserModal: React.FC<RegisterUserModalProps> = ({ isOpen, onClose }) => {
   // Modal content and functionality here
+  if (!isOpen) {
+    return null; // Render nothing if the modal is not open
+  }
 
   return (
     // Modal JSX code
-    <div>
-      {/* Modal content */}
+    <div className="modal">
+      <div className="modal-content">
+        <span className="close-button" onClick={onClose}>
+          &times;
+        </span>
+        <h2>Register as an Individual</h2>
+        {/* Other registration form fields and components */}
+        <button onClick={onClose}>Close</button>
+      </div>
     </div>
   );
 };
